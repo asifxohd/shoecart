497bed7cd4212cd3bab697ff3364c088b2a419b3
Muhammed Asif <asifxohd@gmail.com>
2023-12-02 03:48:46 +0530

    last

--- shoecart/settings.py
+++ shoecart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.212.58.180']
+ALLOWED_HOSTS = ['172.31.82.128']
 
 
 # Application definition
