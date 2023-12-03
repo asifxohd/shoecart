43cd669c409972e982f1e6d1803e9f5e17f0f794
Muhammed Asif <asifxohd@gmail.com>
2023-12-04 00:55:06 +0530

    cc

--- shoecart/settings.py
+++ shoecart/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-#9m%gfl%o7d%g^5kn#o&zxjbg4=)6gyu-3r5d@8ftn!g+&art(')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-#9m%gfl%o7d%g^5kn#o&zxjbg4=)6gyu-3r5d@8ftn!g+&art(')"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
