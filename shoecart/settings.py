e8dcf236d108ee35167762e1f5fd481296dd78dc
Muhammed Asif <140389446+asifxohd@users.noreply.github.com>
2024-03-23 17:35:58 +0530

    additional .env updates

--- shoecart/settings.py
+++ shoecart/settings.py
@@ -97,13 +97,13 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "shoecart",
-        "USER": "postgres",
-        "PASSWORD": "Asif@123",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
