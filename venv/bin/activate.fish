f1c5e23c5e2d95e2c48ca8762c8fe25f9d58bfe9
Muhammed Asif <140389446+asifxohd@users.noreply.github.com>
2024-03-23 17:34:15 +0530

    dependencies updated

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/asifxohd/Desktop/shoecart/venv"
+set -gx VIRTUAL_ENV "/Users/asifxohd/Desktop/FirstProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
